ae1173fce8333409cf90ff2c2b298577ef74241c
David Torres <dtorresm1200@alumno.ipn.mx>
2024-11-09 16:16:00 -0600

    FEATURE: Memoria disponible.

--- src/main/java/com/david/corp/restapi/MainController.java
+++ src/main/java/com/david/corp/restapi/MainController.java
@@ -13,5 +13,25 @@ public class MainController {
     }
     
 
+    @GetMapping("/memory")
+    public String getMemoriaDisponible() {
+        // Obtén la instancia del Runtime
+        Runtime runtime = Runtime.getRuntime();
 
+        // Memoria total disponible para la JVM
+        long totalMemory = runtime.totalMemory();
+        // Memoria libre dentro de la JVM
+        long freeMemory = runtime.freeMemory();
+        // Memoria máxima que la JVM intentará utilizar
+        long maxMemory = runtime.maxMemory();
+
+        // Calcula la memoria disponible real
+        long availableMemory = maxMemory - (totalMemory - freeMemory);
+
+        String result = "Memoria Total (bytes): " + totalMemory
+        +"Memoria Libre (bytes): " + freeMemory
+        +"Memoria Máxima (bytes): " + maxMemory
+        +"Memoria Disponible (bytes): " + availableMemory;
+        return result;
+    }
 }
