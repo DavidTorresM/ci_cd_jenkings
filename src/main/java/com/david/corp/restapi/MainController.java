d6164d651e69740d122b0fb9c9837545d5efced8
David Torres <dtorresm1200@alumno.ipn.mx>
2024-11-09 16:11:57 -0600

    FEATURE: Carga de cpu promedio

--- src/main/java/com/david/corp/restapi/MainController.java
+++ src/main/java/com/david/corp/restapi/MainController.java
@@ -1,6 +1,10 @@
 package com.david.corp.restapi;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,5 +17,22 @@ public class MainController {
     }
     
 
+    @GetMapping("/cpu")
+    public String getCpuUsage() {
+
+        // Obtén una instancia de OperatingSystemMXBean
+        OperatingSystemMXBean osBean = 
+            (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+        
+        // Obtén el uso de la CPU en porcentaje
+        double cpuLoad = osBean.getSystemLoadAverage();
+
+        if (cpuLoad < 0) {
+            return "Uso de CPU no disponible.";
+        } else {
+            return String.format("Carga promedio de la cpu: %.2f%%\n", cpuLoad);
+        }
+    }
+    
 
 }
